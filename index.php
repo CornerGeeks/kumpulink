c714802ce430dc3426430f2a3e95a23ccad41f42
mfirdaus <root@lamp.(none)>
2013-02-23 10:19:22 +0000

    Basic Frontend

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once("header.php");
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>INSERT TITLE HERE</title>
+<title>kumpulink</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
@@ -17,6 +17,12 @@ require_once("header.php");
 <link href="css/bootstrap-responsive.css" rel="stylesheet">
 <script src="js/jquery-1.9.1.js"></script>
 <script src="js/bootstrap.min.js"></script>
+
+<style>
+.inline .btn {
+	margin-bottom:5px;
+}
+</style>
 
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -30,23 +36,89 @@ require_once("header.php");
 
 </head>
 <body>
-<ul class="nav nav-tabs" id="navTab">
-  	  <li><a href="#content1" data-toggle="tab"><i class="icon-home"></i></a></li>
-	  <li><a href="#content2" data-toggle="tab"><i class="icon-user"></i></a></li>
-</ul>
+<div class="navbar">
+  <div class="navbar-inner">
+    <div class="container">
+ 
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+ 
+      <!-- Be sure to leave the brand out there if you want it shown -->
+      <a class="brand" href="#">kumpulink</a>
+ 
+      <!-- Everything you want hidden at 940px or less, place within here -->
+      <div class="nav-collapse collapse">
+      <ul class="nav">
+      <li class="active"><a href="#">Home</a></li>
+	  <li><a href="#">Bookmarklet</a></li>
+	  </ul>
+	 <form class="navbar-form pull-right">
+	  <input type="text" class="span2">
+	  <button type="submit" class="btn">Submit</button>
+	</form>
+      </div>
+ 
+    </div>
+  </div>
+</div>
+
 <div class="container">
-<div class="tab-content">
 
 
-<div id="content1" class="tab-pane active">
-Content here
+<div id="main" class="tab-pane active">
+<div class="media span6">
+  <div class="media">
+  <a class="pull-left" href="http://twitter.com/mfirdaus/"><img src="http://www.google.com/s2/u/0/favicons?domain=plus.google.com"/></a>
+  <div class="media-body">
+    <h4 class="media-heading"><a href="http://twitter.com/mfirdaus/">@mfirdaus at Twitter</a></h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel ligula dolor. Vivamus scelerisque nisi in lorem viverra blandit. Nunc tortor neque, laoreet nec tincidunt nec, hendrerit a ipsum. Donec tristique vehicula ipsum in fermentum. Sed pulvinar tellus ac felis vestibulum pellentesque. Morbi non diam tellus.</p>
+    <ul class="inline">
+    <li>
+	  <div class="btn-group">
+		<button class="btn btn-small">social-media</button>
+		<button class="btn btn-small">x</button>
+	  </div>
+	</li>
+	<li>
+	  <div class="btn-group">
+		<button class="btn btn-small">narcissm</button>
+		<button class="btn btn-small">x</button>
+	  </div>	</li>
+	<li><div class="btn-group">
+		<button class="btn btn-small">meta</button>
+		<button class="btn btn-small">x</button>
+	  </div>	</li>
+	<li>
+	<div class="btn-group">
+		<button class="btn btn-small">social-media</button>
+		<button class="btn btn-small">x</button>
+	  </div>
+	  </li>
+	<li>	<li>
+	<div class="btn-group">
+		<button class="btn btn-small">social-media</button>
+		<button class="btn btn-small">x</button>
+	  </div>
+	  </li>
+	<li>	<li>
+	<div class="btn-group">
+		<button class="btn btn-small">social-media</button>
+		<button class="btn btn-small">x</button>
+	  </div>
+	  </li>
+	</ul>
+	<div class="input-append input-block-level">
+	  <input class="span2" type="text" placeholder="Add tags, seperated by commas.">
+	  <button class="btn" type="button">+</button>
+	</div>
+  </div>
 </div>
+  
 
-<div id="content2" class="tab-pane">
-Content 2 here
 </div>
-
-</div> <!--tabcontent-->
-</div> <!--container-->
+</div>
 </body>
 </html>
